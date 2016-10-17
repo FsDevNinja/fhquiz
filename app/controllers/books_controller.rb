8e20c81f25ed1a1c1a77dfee8f33d3ba54670c72
Matt Taylor <makproductions06@gmail.com>
2016-10-16 21:22:41 -0400

    added simpleform and ability to create and display new books

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,24 @@
+class BooksController < ApplicationController
+  
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+  
+  def create
+    Book.create(book_params)
+    redirect_to root_path
+  end
+  
+  
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title,:author)
+    
+  end
+  
+  
+  
+end
